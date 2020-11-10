f7c6bfffddb527cbcc218181660f4f9a85b23673
David Greaves <2014269@gmail.com>
2020-11-10 09:32:07 -0600

    Adding basic contacts function

--- models/contact.py
+++ models/contact.py
@@ -6,13 +6,14 @@ from typing import Dict
 
 
 @dataclass(eq=False)
-class Original(Model, metaclass=ABCMeta):
+class Contact(Model, metaclass=ABCMeta):
     collection: str = field(init=False, default="contacts")
     name_first: str
     name_last: str
     phone: str
     email: str
     address: str
+    user_email: str
     _id: str = field(default_factory=lambda: uuid.uuid4().hex)
 
     def json(self) -> Dict:
@@ -22,5 +23,6 @@ class Original(Model, metaclass=ABCMeta):
             "phone": self.phone,
             "email": self.email,
             "address": self.address,
+            "user_email": self.user_email,
             "_id": self._id
         }
--- models/original.py
+++ models/original.py
@@ -4,7 +4,7 @@ from models.model import Model
 import uuid
 from typing import Dict
 import datetime
-
+from models.user import User
 
 @dataclass(eq=False)
 class Original(Model, metaclass=ABCMeta):
@@ -18,7 +18,11 @@ class Original(Model, metaclass=ABCMeta):
     possession: str
     has_prints: str
     is_commission: str
+    user_email: str
     _id: str = field(default_factory=lambda: uuid.uuid4().hex)
+
+    def __post_init__(self):
+        self.user = User.find_by_email(self.user_email)
 
     def json(self) -> Dict:
         return {
@@ -31,5 +35,6 @@ class Original(Model, metaclass=ABCMeta):
             "possession": self.possession,
             "has_prints": self.has_prints,
             "is_commission": self.is_commission,
+            "user_email": self.user_email,
             "_id": self._id
         }
