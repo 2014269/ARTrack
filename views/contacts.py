c1a14f8fffdea4e6e5def15ad1f281ac9a228162
David Greaves <2014269@gmail.com>
2020-11-10 17:08:59 -0600

    Added login decorator, messages

---
+++ views/contacts.py
@@ -0,0 +1,81 @@
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from models.original import Original
+from models.contact import Contact
+from flask import session
+from models.user import requires_login
+
+contact_blueprint = Blueprint("contacts", __name__)
+
+
+@contact_blueprint.route("/")
+@requires_login
+def index():
+    contacts = Contact.find_many_by('user_email', session['email'])
+    return render_template("contacts/index.html", contacts=contacts)
+
+
+@contact_blueprint.route("/new", methods=['GET', 'POST'])
+@requires_login
+def new_contact():
+    if request.method == "POST":
+        # Populate the Data
+        name_first = request.form["name_first"]
+        name_last = request.form["name_last"]
+        phone = request.form["phone"]
+        email = request.form["email"]
+        address = request.form["address"]
+        user_email = session['email']
+
+        # Initialize Object and save to database
+        Contact(name_first,
+                name_last,
+                phone,
+                email,
+                address,
+                user_email).save_to_mongo()
+
+        # Future: some sort of confirmation that this worked
+
+    return render_template("contacts/new_contact.html")
+
+
+@contact_blueprint.route("/edit/<string:contact_id>", methods=['GET', 'POST'])
+@requires_login
+def edit_contact(contact_id):
+    if request.method == "POST":
+        # Populate the Data
+        name_first = request.form["name_first"]
+        name_last = request.form["name_last"]
+        phone = request.form["phone"]
+        email = request.form["email"]
+        address = request.form["address"]
+
+        contact = Contact.get_by_id(contact_id)
+
+        # Update data and save
+        contact.name_first = name_first
+        contact.name_last = name_last
+        contact.phone = phone
+        contact.email = email
+        contact.address = address
+        contact.save_to_mongo()
+
+        # Future: some sort of confirmation that this worked
+        return redirect(url_for('contacts.details_contact', contact_id=contact_id))
+
+    return render_template("contacts/edit_contact.html", contact=Contact.get_by_id(contact_id))
+
+
+@contact_blueprint.route("/delete/<string:contact_id>")
+@requires_login
+def delete_contact(contact_id):
+    contact = Contact.get_by_id(contact_id)
+    if contact.user_email == session['email']:
+        contact.remove_from_mongo()
+    return redirect(url_for(".index"))
+
+
+@contact_blueprint.route("/details/<string:contact_id>")
+@requires_login
+def details_contact(contact_id):
+    return render_template("contacts/details.html", contact=Contact.get_by_id(contact_id))
--- views/originals.py
+++ views/originals.py
@@ -1,16 +1,23 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from models.original import Original
 from flask import session
+from models.user import requires_login
+
+
+
 original_blueprint = Blueprint("originals", __name__)
 
 
 @original_blueprint.route("/")
+@requires_login
 def index():
-    originals = Original.all()
+    originals = Original.find_many_by('user_email', session['email'])
     return render_template("originals/index.html", originals=originals)
 
 
+
 @original_blueprint.route("/new", methods=['GET', 'POST'])
+@requires_login
 def new_original():
     if request.method == "POST":
         # Populate the Data
@@ -23,6 +30,7 @@ def new_original():
         possession = request.form["possession"]
         has_prints = request.form["has_prints"]
         is_commission = request.form["is_commission"]
+        user_email = session['email']
 
         # Initialize Object and save to database
         Original(title,
@@ -33,7 +41,8 @@ def new_original():
                  status,
                  possession,
                  has_prints,
-                 is_commission).save_to_mongo()
+                 is_commission,
+                 user_email).save_to_mongo()
 
         # Future: some sort of confirmation that this worked
 
@@ -41,6 +50,7 @@ def new_original():
 
 
 @original_blueprint.route("/edit/<string:original_id>", methods=['GET', 'POST'])
+@requires_login
 def edit_original(original_id):
     if request.method == "POST":
         # Populate the Data
@@ -75,12 +85,15 @@ def edit_original(original_id):
 
 
 @original_blueprint.route("/delete/<string:original_id>")
+@requires_login
 def delete_original(original_id):
     original = Original.get_by_id(original_id)
-    original.remove_from_mongo()
+    if original.user_email == session['email']:
+        original.remove_from_mongo()
     return redirect(url_for(".index"))
 
 
 @original_blueprint.route("/details/<string:original_id>")
+@requires_login
 def details_original(original_id):
     return render_template("originals/details.html", original=Original.get_by_id(original_id))
